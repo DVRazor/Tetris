840bcae4c5566d849a332861724314b0743ffd43
DVRazor <dvrazor@mail.ru>
2017-04-07 15:48:07 +0300

    Added reset for easeljs objects

--- js/CreatejsParticleManager.js
+++ js/CreatejsParticleManager.js
@@ -3,18 +3,37 @@ var CreatejsParticleManager = function(AM){
 
 	ParticleManager.call(scope);
 
+	scope.containers = {};
+
 	var addParticle = scope.addParticle;
 	var removeParticle = scope.removeParticle;
 
-	scope.addParticle = function(type, obj, init, container){
-		addParticle(type, obj, init);
-		container.addChild(obj);
+	scope.addParticle = function(typeName, container, params){
+		registerContainer(container);
+
+		var particle = addParticle(typeName, params);
+		container.addChild(particle);
 	}
 
-	scope.removeParticle = function(particle){		
-		// debugger;
+	scope.removeParticle = function(particle){	
 		removeParticle(particle);
-		particle.parent.removeChild(particle)
+		particle.parent.removeChild(particle);
+
+		resetObject(particle);
 		AM.put(particle);		
-	}	
+	};
+
+	function resetObject(obj){
+		obj.x = 0;
+		obj.y = 0;
+		obj.scaleX = 1;
+		obj.scaleY = 1;
+		obj.alpha = 1;
+		obj.rotation = 0;
+	}
+
+	function registerContainer(container){
+		var id = container.id;
+		scope.containers[id] = container;		
+	}
 }
